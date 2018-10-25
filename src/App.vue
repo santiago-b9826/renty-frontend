895f051417546d1e931e450b7d329bd0926b95e8
Angelica <angelica.arroyavem@udea.edu.co>
2018-10-24 20:59:45 -0500

    Making more details

--- src/App.vue
+++ src/App.vue
@@ -48,11 +48,13 @@ position:absolute;
   text-align: center;
 }
 .filter{
+  width: 1020px;
+  height: 360px;
   margin: 0;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin-right: -50%;
-    transform: translate(-50%, -50%)
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%)
 }
 </style>
--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,16 +1,73 @@
 <template>
-  <div class="card">
-  <h5 class="card-header">Featured</h5>
-  <div class="card-body">
-    <div class='input-group date' id='datetimepicker1'>
-        <input type='text' class="form-control" />
-        <span class="input-group-addon">
-            <span class="glyphicon glyphicon-calendar"></span>
-        </span>
+  <div class=" card">
+    <h5 class="card-header">Busca el auto que necesitas:</h5>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm filters">
+          <div class="dropdown show">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Fecha de 
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm filters">
+          <div class="dropdown show">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown link
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+          <div class="col-sm filters">
+          <div class="dropdown show">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown link
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div>
+         <div class="col-sm filters">
+          <div class="dropdown show">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown link
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+          <div class="col-sm filters">
+          </div>
+          <div class="col-sm filters">
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
     </div>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-</div>
+
 </template>
 
 <script>
@@ -20,8 +77,11 @@ export default {
     msg: String
   }
 };
-
- $(function () {
-                $('#datetimepicker1').datetimepicker();
-            });
 </script>
+
+<style>
+.filters{
+  margin-top: 40px;
+  margin-bottom: 5px;
+}
+</style>
